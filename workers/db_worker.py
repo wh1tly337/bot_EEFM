eeda339df1e5dba9e6cd26701976eef68270d841
wh1tly337 <wh1tly337@gmail.com>
2023-02-14 15:32:35 +0500

    Реализована функция удаления пользователя из бд

--- workers/db_worker.py
+++ workers/db_worker.py
@@ -14,7 +14,6 @@ async def start_connection():
     try:
         connection = sqlite3.connect(src_db)
         cursor = connection.cursor()
-        # logger.info('SQL connection started')
     except Exception as ex:
         logger.error(ex)
 
@@ -26,7 +25,6 @@ async def close_connection():
     try:
         if connection:
             connection.close()
-            # logger.info('SQL connection closed')
     except Exception as ex:
         logger.error(ex)
 
@@ -53,8 +51,6 @@ async def get_data():
     try:
         await start_connection()
         cursor.execute(f"""SELECT * FROM users""")
-        test = cursor.fetchall()
-        print(test)
         # TODO сразу здесь реализовать через fetchall что и откуда мы хотим получить через if/else
         # return *значение*
         await close_connection()
@@ -62,6 +58,7 @@ async def get_data():
         logger.error(ex)
 
 
+# функция для добавления нового пользователя в бд
 async def add_new_user(id_tg, first_name, username, post, date_added, date_removed):
     await start_connection()
     cursor.execute(
@@ -72,7 +69,16 @@ async def add_new_user(id_tg, first_name, username, post, date_added, date_remov
     logger.info(f'Added new user to table users | {id_tg}, {first_name}, {username}, {post}')
     await close_connection()
 
-# TODO async def remove_user():
+
+# функция для удаления пользователя из бд
+async def remove_user(id_tg):
+    await start_connection()
+    cursor.execute(
+        f"""DELETE FROM users WHERE id = '{id_tg}'""",
+    )
+    connection.commit()
+    logger.info(f'Delete user from table users | {id_tg}')
+    await close_connection()
 
 # TODO реализовать функцию перевода из Excel файла в sql таблицу (schedule созданную отдельно, один раз)
 #  при помощи обновления таблицы созданную только для расписания (связать с таблицей users при помощи primary key или id пользователя)
