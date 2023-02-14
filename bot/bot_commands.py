6c87d184b1fa793619b6fe23a5dd5b7768e9968f
Александр Шешенин <s.sheshenin@yandex.ru>
2023-02-14 15:59:56 +0500

    Refactoring code

--- bot/bot_commands.py
+++ bot/bot_commands.py
@@ -13,13 +13,34 @@ database = []  # временная бд для проверки всего ос
 @dp.message_handler(commands=['start'])
 async def start_message(message: types.Message):
     if message.from_user.id not in database:
-        logger.info(f"New user start bot | {message.from_user.id}, {message.from_user.full_name}, {message.from_user.username}")
-        await bot_aiogram.send_message(chat_id=message.chat.id, text=f"{message.from_user.full_name}, к сожалению у вас нет доступа к данному боту", reply_markup=markup_new_user)
+        # метод начинает считывание последующего сообщения пользователя
+        # для его авторизации 
+        # (нужен для работы функции authorization_handler в файле message_handler.py)
+        logger.info((
+            "New user start bot |",
+            message.from_user.id,
+            message.from_user.full_name,
+            message.from_user.username,
+        ))
+        msg_dont_have_perm = ' , к сожалению у вас нет доступа к данному боту'
+        await bot_aiogram.send_message(
+            chat_id=message.chat.id,
+            text=message.from_user.full_name + msg_dont_have_perm,
+            reply_markup=markup_new_user,
+        )
         await mh.Response.authorization_handler.set()
-        # метод выше начинает считывание последующего сообщения пользователя для его авторизации (нужен для работы функции authorization_handler в файле message_handler.py)
     else:
-        logger.info(f"User log in | {message.from_user.id}, {message.from_user.full_name}, {message.from_user.username}")
-        await bot_aiogram.send_message(chat_id=message.chat.id, text=f"{message.from_user.full_name}, добро пожаловать в бот клиники ИТА!", reply_markup=markup_test)
+        logger.info(("User log in |",
+        message.from_user.id,
+        message.from_user.full_name,
+        message.from_user.username,
+        ))
+        msg_hello_new_doc = " , добро пожаловать в бот клиники ИТА!"
+        await bot_aiogram.send_message(
+            chat_id=message.chat.id,
+            text={message.from_user.full_name} + msg_hello_new_doc,
+            reply_markup=markup_test
+        )
 
 
 # регистратор передающий данные в main_bot.py
--- bot/message_handler.py
+++ bot/message_handler.py
@@ -6,46 +6,92 @@ from auxiliary.all_markups import *
 from auxiliary.req_data import *
 
 
-# класс для регистрации состояния сообщений пользователя (можете сильно не вникать это просто необходимо для правильной работы, просто копируйте и меняйте названия переменных под свою задачу)
+# класс для регистрации состояния сообщений пользователя 
+# (можете сильно не вникать это просто необходимо для правильной работы, 
+# просто копируйте и меняйте названия переменных под свою задачу)
 class Response(StatesGroup):
-    authorization_handler = State()
+         = State()
     authorization_password_handler = State()
 
 
-# функция для обработки сообщения после нажатия кнопки "Получить доступ", вызывается из файла bot_commands.py строка 16
+# функция для обработки сообщения после нажатия кнопки "Получить доступ", 
+# вызывается из файла bot_commands.py строка 
 async def authorization_handler(message: types.Message, state: FSMContext):
     authorization_response = message.text
     await state.update_data(user_response=authorization_response)
 
     if authorization_response == 'Отмена':
-        await bot_aiogram.send_message(chat_id=message.chat.id, text='Хорошо', parse_mode='Markdown', reply_markup=markup_new_user)
+        await bot_aiogram.send_message(
+            chat_id=message.chat.id,
+            text='Хорошо',
+            parse_mode='Markdown',
+            reply_markup=markup_new_user
+        )
         await state.finish()
     else:
         message_text = f"Введите временный пароль. Его нужно уточнить у {boss}"
-        await bot_aiogram.send_message(chat_id=message.chat.id, text=message_text, parse_mode='Markdown', reply_markup=markup_cancel)
+        await bot_aiogram.send_message(
+            chat_id=message.chat.id,
+            text=message_text,
+            parse_mode='Markdown',
+            reply_markup=markup_cancel
+        )
         await Response.authorization_password_handler.set()
 
 
 # функция для обработки временного пароля, вызывается в 26 строке этого файла
 async def authorization_password_handler(message: types.Message, state: FSMContext):
     authorization_password_response = message.text
-    await state.update_data(user_response=authorization_password_response)
+    await state.update_data(
+        user_response=authorization_password_response
+    )
 
     if authorization_password_response == 'Отмена':
-        await bot_aiogram.send_message(chat_id=message.chat.id, text='Хорошо', parse_mode='Markdown', reply_markup=markup_new_user)
+        await bot_aiogram.send_message(
+            chat_id=message.chat.id, text='Хорошо',
+            parse_mode='Markdown',
+            reply_markup=markup_new_user
+        )
         await state.finish()
     else:
         if authorization_password_response == temporary_password:
-            await bot_aiogram.send_message(chat_id=message.chat.id, text=f"{message.from_user.full_name}, добро пожаловать в бот клиники ИТА!", parse_mode='Markdown', reply_markup=markup_test)
-            logger.info(f"New user log in and added to database | {message.from_user.id}, {message.from_user.full_name}, {message.from_user.username}")
+            msg_hello_new_user = " , добро пожаловать в бот клиники ИТА!"
+            await bot_aiogram.send_message(
+                chat_id=message.chat.id,
+                text=message.from_user.full_name + msg_hello_new_user,
+                parse_mode='Markdown',
+                reply_markup=markup_test
+            )
+            logger.info((
+                "New user log in and added to database |",
+                message.from_user.id,
+                message.from_user.full_name,
+                message.from_user.username
+            ))
         else:
-            await bot_aiogram.send_message(chat_id=message.chat.id, text='Пароль введен неверно', parse_mode='Markdown', reply_markup=markup_new_user)
-            logger.info(f"User unsuccessfully tried to log in | {message.from_user.id}, {message.from_user.full_name}, {message.from_user.username}")
+            await bot_aiogram.send_message(
+                chat_id=message.chat.id,
+                text='Пароль введен неверно',
+                parse_mode='Markdown',
+                reply_markup=markup_new_user
+            )
+            logger.info((
+                "User unsuccessfully tried to log in |",
+                message.from_user.id,
+                message.from_user.full_name,
+                message.from_user.username
+            ))
 
     await state.finish()
 
 
 # регистратор передающий данные в main_bot.py
 def register_handlers_authorization(dp: Dispatcher):  # noqa
-    dp.register_message_handler(authorization_handler, state=Response.authorization_handler)
-    dp.register_message_handler(authorization_password_handler, state=Response.authorization_password_handler)
+    dp.register_message_handler(
+        authorization_handler,
+        state=Response.authorization_handler
+    )
+    dp.register_message_handler(
+        authorization_password_handler,
+        state=Response.authorization_password_handler
+    )
